34d7073ea0194ad41df220fd2c7914c9cf47c635
Davo Smith <git@davosmith.co.uk>
2011-02-09 20:41:19 +0000

    Trying a different method of generating the next/prev details

--- footer.php
+++ footer.php
@@ -5,10 +5,7 @@ require_once(dirname(__FILE__).'/definitions.php');
 echo '<!-- Navbuttons start -->';
 
 $display = true;
-if (!isset($THEME->menu) || $THEME->menu == '') {
-    echo '<!-- No menu -->';
-    $display = false;
-} elseif ($COURSE->id <= 1) {
+if ($COURSE->id <= 1) {
     echo '<!-- Front page -->';
     $display = false;
 } elseif (!$settings = get_record('navbuttons', 'course', $COURSE->id)) {
@@ -17,183 +14,198 @@ if (!isset($THEME->menu) || $THEME->menu == '') {
 } elseif (!$settings->enabled) {
     echo '<!-- Not enabled -->';
     $display = false;
+} elseif (!isset($THEME->cm) || $THEME->cm == NULL) {
+    echo '<!-- No cmid -->';
+    $display = false;
 }
 
 if ($display) {
+    $cmid = $THEME->cm->id;
 
-    echo '<!-- '.$THEME->menu.' -->';
+    $modinfo = get_fast_modinfo($COURSE);
+    $context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
+    $sections = get_records('course_sections', 'course', $COURSE->id, 'section', 'section,visible,summary');
     
-    $dom = new domDocument;
-    $dom->loadHTML($THEME->menu);
-    $dom->preserveWhiteSpace = false;
+    $next = false;
+    $prev = false;
+    $firstcourse = false;
+    $firstsection = false;
+    $lastcourse = false;
+    $lastsection = false;
 
-    $menu = $dom->getElementById('navmenupopup');
-    if ($menu) {
-        $options = $menu->getElementsByTagName('option');
+    $sectionnum = -1;
+    $thissection = null;
+    $firstthissection = false;
+    $flag = false;
+    $sectionflag = false;
+    $previousmod = false;
 
-        $next = false;
-        $prev = false;
-        $firstcourse = false;
-        $firstsection = false;
-        $lastcourse = false;
-        $lastsection = false;
-
-        if ($options->length > 0) {
-            $firstactivity = $options->item(0);
-            $firstcourse = new stdClass;
-            $firstcourse->link = $firstactivity->getAttribute('value');
-            $firstcourse->name = $firstactivity->nodeValue;
-
-            $lastactivity = $options->item($options->length - 1);
-            $lastcourse = new stdClass;
-            $lastcourse->link = $lastactivity->getAttribute('value');
-            $lastcourse->name = $lastactivity->nodeValue;
+    foreach ($modinfo->cms as $mod) {
+        if ($mod->modname == 'label') {
+            continue;
         }
 
-        // Find the next/previous activty & first/last in course
-        foreach ($options as $pos => $option) {
-            if ($option->hasAttribute('selected')) {
-                if ($pos > 0) {
-                    $prevactivity = $options->item($pos - 1);
-                    $prev = new stdClass;
-                    $prev->link = $prevactivity->getAttribute('value');
-                    $prev->name = $prevactivity->nodeValue;
-                } else {
-                    $firstcourse = false;
-                }
-
-                if ($pos < $options->length-1) {
-                    $nextactivity = $options->item($pos + 1);
-                    $next = new stdClass;
-                    $next->link = $nextactivity->getAttribute('value');
-                    $next->name = $nextactivity->nodeValue;
-                } else if ($pos == $options->length-1) {
-                    $lastcourse = false;
-                }
-            }
+        if ($mod->sectionnum > $COURSE->numsections) {
+            break;
         }
 
-        // Find first / last activity in section
-        $optgroups = $menu->getElementsByTagName('optgroup');
-        foreach ($optgroups as $optgroup) {
-            $options = $optgroup->getElementsByTagName('option');
-            foreach ($options as $pos => $option) {
-                if ($option->hasAttribute('selected')) {
-                    if ($pos > 0) {
-                        $firstactivity = $options->item(0);
-                        $firstsection = new stdClass;
-                        $firstsection->link = $firstactivity->getAttribute('value');
-                        $firstsection->name = $firstactivity->nodeValue;
+        if (!$mod->uservisible) {
+            continue;
+        }
+
+        if ($mod->sectionnum > 0 && $sectionnum != $mod->sectionnum) {
+            $thissection = $sections[$mod->sectionnum];
+
+            if ($thissection->visible || !$COURSE->hiddensections ||
+                has_capability('moodle/course:viewhiddensections', $context)) {
+                $sectionnum = $mod->sectionnum;
+                $firstthissection = false;
+                if ($sectionflag) {
+                    if ($flag) { // flag means selected mod was the last in the section
+                        $lastsection = 'none';
+                    } else {
+                        $lastsection = $previousmod;
                     }
-
-                    if ($pos < $options->length -1) {
-                        $lastactivity = $options->item($options->length - 1);
-                        $lastsection = new stdClass;
-                        $lastsection->link = $lastactivity->getAttribute('value');
-                        $lastsection->name = $lastactivity->nodeValue;
-                    }
-
-                    break 2;
-                }
-            }
-        }
-
-        echo '<div id="navbuttons" style="float: right; width: 400px; right: 0; margin-top: 5px;">';
-        if ($settings->homebuttonshow) {
-            $home = new stdClass;
-            if ($settings->homebuttontype == BLOCK_NAVBUTTONS_HOME_COURSE) {
-                $home->link = $CFG->wwwroot.'/course/view.php?id='.$COURSE->id;
-                $home->name = get_string('coursepage','block_navbuttons');
-            } else {
-                $home->link = $CFG->wwwroot;
-                $home->name = get_string('frontpage','block_navbuttons');
-            }
-            list($icon, $bgcolour) = navbutton_get_icon('home.png', $settings->homebuttonicon, $settings->backgroundcolour, $settings->customusebackground);
-            echo make_navbutton($icon, $bgcolour, $home->name, $home->link);
-        }
-
-        if ($settings->firstbuttonshow) {
-            $first = new stdClass;
-            if ($settings->firstbuttontype == BLOCK_NAVBUTTONS_FIRST_IN_COURSE) {
-                if (!$firstcourse) {
-                    $first = false;
-                } else {
-                    $first->name = get_string('firstcourse','block_navbuttons').': '.$firstcourse->name;
-                    $first->link = $firstcourse->link;
-                }
-            } elseif ($settings->firstbuttontype == BLOCK_NAVBUTTONS_FIRST_IN_SECTION) {
-                if (!$firstsection) {
-                    $first = false;
-                } else {
-                    $first->name = get_string('firstsection','block_navbuttons').': '.$firstsection->name;
-                    $first->link = $firstsection->link;
+                    $sectionflag = false;
                 }
             } else {
-                $first->name = get_string('coursepage','block_navbuttons');
-                $first->link = $CFG->wwwroot.'/course/view.php?id='.$COURSE->id;
-            }
-            if ($first) {
-                list($icon, $bgcolour) = navbutton_get_icon('first.png', $settings->firstbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
-                echo make_navbutton($icon, $bgcolour, $first->name, $first->link);
+                continue;
             }
         }
 
-        if ($settings->prevbuttonshow && $prev) {
-            list($icon, $bgcolour) = navbutton_get_icon('prev.png', $settings->prevbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
-            echo make_navbutton($icon, $bgcolour, get_string('prevactivity','block_navbuttons').': '.$prev->name, $prev->link);
+        $thismod = new stdClass;
+        $thismod->link = $CFG->wwwroot.'/mod/'.$mod->modname.'/view.php?id='.$mod->id;
+        $thismod->name = strip_tags(format_string($mod->name,true));
+        
+        if ($flag) { // Current mod is the 'next' mod
+            $next = $thismod;
+            $flag = false;
         }
-        if ($settings->nextbuttonshow && $next) {
-            list($icon, $bgcolour) = navbutton_get_icon('next.png', $settings->nextbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
-            echo make_navbutton($icon, $bgcolour, get_string('nextactivity','block_navbuttons').': '.$next->name, $next->link);
-        }
-
-        if ($settings->lastbuttonshow) {
-            $last = new stdClass;
-            if ($settings->lastbuttontype == BLOCK_NAVBUTTONS_LAST_IN_COURSE) {
-                if (!$lastcourse) {
-                    $last = false;
-                } else {
-                    $last->name = get_string('lastcourse','block_navbuttons').': '.$lastcourse->name;
-                    $last->link = $lastcourse->link;
-                }
-            } elseif ($settings->lastbuttontype == BLOCK_NAVBUTTONS_LAST_IN_SECTION) {
-                if (!$lastsection) {
-                    $last = false;
-                } else {
-                    $last->name = get_string('lastsection','block_navbuttons').': '.$lastsection->name;
-                    $last->link = $lastsection->link;
-                }
-            } else {
-                $last->name = get_string('coursepage','block_navbuttons');
-                $last->link = $CFG->wwwroot.'/course/view.php?id='.$COURSE->id;
-            }
-            if ($last) {
-                list($icon, $bgcolour) = navbutton_get_icon('last.png', $settings->lastbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
-                echo make_navbutton($icon, $bgcolour, $last->name, $last->link);
+        if ($cmid == $mod->id) {
+            $flag = true;
+            $sectionflag = true;
+            $prev = $previousmod;
+            $firstsection = $firstthissection;
+            if (!$firstcourse) {
+                $firstcourse = 'none'; // Prevent the 'firstcourse' link if this is the first item
             }
         }
-
-        if ($settings->extra1show && $settings->extra1link) {
-            list($icon, $bgcolour) = navbutton_get_icon('extra1.png', $settings->extra1icon, $settings->backgroundcolour, $settings->customusebackground);
-            if (!$settings->extra1title) {
-                $settings->extra1title = $settings->extra1link;
-            }
-            echo make_navbutton($icon, $bgcolour, $settings->extra1title, $settings->extra1link, $settings->extra1openin);
+        if (!$firstthissection) {
+            $firstthissection = $thismod;
+        }
+        if (!$firstcourse) {
+            $firstcourse = $thismod;
         }
 
-        if ($settings->extra2show && $settings->extra2link) {
-            list($icon, $bgcolour) = navbutton_get_icon('extra2.png', $settings->extra2icon, $settings->backgroundcolour, $settings->customusebackground);
-            if (!$settings->extra2title) {
-                $settings->extra2title = $settings->extra2link;
-            }
-            echo make_navbutton($icon, $bgcolour, $settings->extra2title, $settings->extra2link, $settings->extra2openin);
-        }
-
-        echo '</div>';
-        echo '<br style="clear:both;" />';
-    } else {
-        echo '<!-- Wrong menu type -->';
+        $previousmod = $thismod;
     }
+    if (!$flag) { // flag means selected mod is the last in the course
+        if (!$lastsection) {
+            $lastsection = $previousmod;
+        }
+        $lastcourse = $previousmod;
+    }
+    if ($firstcourse == 'none') {
+        $firstcourse = false;
+    }
+    if ($lastsection == 'none') {
+        $lastsection = false;
+    }
+    
+
+    echo '<div id="navbuttons" style="float: right; width: 400px; right: 0; margin-top: 5px;">';
+    if ($settings->homebuttonshow) {
+        $home = new stdClass;
+        if ($settings->homebuttontype == BLOCK_NAVBUTTONS_HOME_COURSE) {
+            $home->link = $CFG->wwwroot.'/course/view.php?id='.$COURSE->id;
+            $home->name = get_string('coursepage','block_navbuttons');
+        } else {
+            $home->link = $CFG->wwwroot;
+            $home->name = get_string('frontpage','block_navbuttons');
+        }
+        list($icon, $bgcolour) = navbutton_get_icon('home.png', $settings->homebuttonicon, $settings->backgroundcolour, $settings->customusebackground);
+        echo make_navbutton($icon, $bgcolour, $home->name, $home->link);
+    }
+
+    if ($settings->firstbuttonshow) {
+        $first = new stdClass;
+        if ($settings->firstbuttontype == BLOCK_NAVBUTTONS_FIRST_IN_COURSE) {
+            if (!$firstcourse) {
+                $first = false;
+            } else {
+                $first->name = get_string('firstcourse','block_navbuttons').': '.$firstcourse->name;
+                $first->link = $firstcourse->link;
+            }
+        } elseif ($settings->firstbuttontype == BLOCK_NAVBUTTONS_FIRST_IN_SECTION) {
+            if (!$firstsection) {
+                $first = false;
+            } else {
+                $first->name = get_string('firstsection','block_navbuttons').': '.$firstsection->name;
+                $first->link = $firstsection->link;
+            }
+        } else {
+            $first->name = get_string('coursepage','block_navbuttons');
+            $first->link = $CFG->wwwroot.'/course/view.php?id='.$COURSE->id;
+        }
+        if ($first) {
+            list($icon, $bgcolour) = navbutton_get_icon('first.png', $settings->firstbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
+            echo make_navbutton($icon, $bgcolour, $first->name, $first->link);
+        }
+    }
+
+    if ($settings->prevbuttonshow && $prev) {
+        list($icon, $bgcolour) = navbutton_get_icon('prev.png', $settings->prevbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
+        echo make_navbutton($icon, $bgcolour, get_string('prevactivity','block_navbuttons').': '.$prev->name, $prev->link);
+    }
+    if ($settings->nextbuttonshow && $next) {
+        list($icon, $bgcolour) = navbutton_get_icon('next.png', $settings->nextbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
+        echo make_navbutton($icon, $bgcolour, get_string('nextactivity','block_navbuttons').': '.$next->name, $next->link);
+    }
+
+    if ($settings->lastbuttonshow) {
+        $last = new stdClass;
+        if ($settings->lastbuttontype == BLOCK_NAVBUTTONS_LAST_IN_COURSE) {
+            if (!$lastcourse) {
+                $last = false;
+            } else {
+                $last->name = get_string('lastcourse','block_navbuttons').': '.$lastcourse->name;
+                $last->link = $lastcourse->link;
+            }
+        } elseif ($settings->lastbuttontype == BLOCK_NAVBUTTONS_LAST_IN_SECTION) {
+            if (!$lastsection) {
+                $last = false;
+            } else {
+                $last->name = get_string('lastsection','block_navbuttons').': '.$lastsection->name;
+                $last->link = $lastsection->link;
+            }
+        } else {
+            $last->name = get_string('coursepage','block_navbuttons');
+            $last->link = $CFG->wwwroot.'/course/view.php?id='.$COURSE->id;
+        }
+        if ($last) {
+            list($icon, $bgcolour) = navbutton_get_icon('last.png', $settings->lastbuttonicon, $settings->backgroundcolour, $settings->customusebackground);
+            echo make_navbutton($icon, $bgcolour, $last->name, $last->link);
+        }
+    }
+
+    if ($settings->extra1show && $settings->extra1link) {
+        list($icon, $bgcolour) = navbutton_get_icon('extra1.png', $settings->extra1icon, $settings->backgroundcolour, $settings->customusebackground);
+        if (!$settings->extra1title) {
+            $settings->extra1title = $settings->extra1link;
+        }
+        echo make_navbutton($icon, $bgcolour, $settings->extra1title, $settings->extra1link, $settings->extra1openin);
+    }
+
+    if ($settings->extra2show && $settings->extra2link) {
+        list($icon, $bgcolour) = navbutton_get_icon('extra2.png', $settings->extra2icon, $settings->backgroundcolour, $settings->customusebackground);
+        if (!$settings->extra2title) {
+            $settings->extra2title = $settings->extra2link;
+        }
+        echo make_navbutton($icon, $bgcolour, $settings->extra2title, $settings->extra2link, $settings->extra2openin);
+    }
+
+    echo '</div>';
+    echo '<br style="clear:both;" />';
 }
 
 function navbutton_get_icon($default, $usericon, $bgcolour, $customusebackground) {
