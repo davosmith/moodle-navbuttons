b1be57739741dd78f16f7272b8ac637008298ccc
Davo Smith <git@davosmith.co.uk>
2011-02-16 19:12:09 +0000

    Added German translation from Joachim Vogelgesang

---
+++ lang/de/block_navbuttons.php
@@ -0,0 +1,36 @@
+<?php
+
+$string['backgroundcolour'] = 'Symbol Farbe (jede g&uuml;ltige HTML Farbe)';
+$string['buttonicon'] = 'Benutzerdefiniertes Symbol laden (leer lassen f&uuml;r Standard)';
+$string['buttonlink'] = 'URL dieser Taste zuordnen';
+$string['buttonopenin'] = 'Link &ouml;ffnet in';
+$string['buttontitle'] = 'Tasten Titel';
+$string['buttontype'] = 'Taste f&uuml;hrt zu';
+$string['buttonsenabled'] = 'Navigationstasten aktiviert f&uuml;r diesen Kurs';
+$string['coursepage'] = 'Kurs Seite';
+$string['customusebackground'] = 'Farbe f&uuml;r eigene Symbole';
+$string['displaybutton'] = 'Diese Taste';
+$string['editsettings'] = 'Einstellungen der Navigationstaste bearbeiten';
+$string['extra1'] = 'Extra Taste 1';
+$string['extra2'] = 'Extra Taste 2';
+$string['firstbutton'] = 'Erste Taste';
+$string['firstcourse'] = 'Erste Aktivit&auml;t im Kurs';
+$string['firstsection'] = 'Erste Aktivit&auml;t im Abschnitt';
+$string['frontpage'] = 'Titelseite';
+$string['generalsettings'] = 'Allgemeine Einstellungen';
+$string['homebutton'] = 'Home Taste';
+$string['lastactivity'] = 'Letzte Aktivit&auml;t';
+$string['lastactivitysection'] = 'Letzte Aktivit&auml;t im Abschnitt';
+$string['lastbutton'] = 'Letzte Taste';
+$string['lastcourse'] = 'Letzte Aktivit&auml;t im Kurs';
+$string['lastsection'] = 'Letzte Aktivit&auml;t im Abschnitt';
+$string['navbuttons'] = 'Navigationstasten';
+$string['nextactivity'] = 'N&auml;chste Aktivit&auml;t';
+$string['nextbutton'] = 'N&auml;chste Taste';
+$string['pluginname'] = 'Navigationstasten';
+$string['prevactivity'] = 'Vorherige Aktivit&auml;t';
+$string['prevbutton'] = 'Vorherige Taste';
+$string['selftest_nofooter'] = 'Navigationstaste Selbsttest: Die Zeile \"include(\$CFG->dirroot.\'/blocks/navbuttons/footer.php\');\" wurde nicht korrekt weblib.php zugeordnet - siehe README.txt f&uuml;r weitere Anweisungen';
+$string['selftest_ok'] = 'Navigationstaste Selbsttest: erforderliche Kern Änderungen wurden erfolgreich abgeschlossen.';
+$string['settingsupdated'] = 'Einstellungen aktualisiert';
+?>
