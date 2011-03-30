4a4913fec8704d433bd7db57728013139be2433d
Davo Smith <git@davosmith.co.uk>
2011-03-30 18:40:03 +0100

    Added Spanish translation from Victor Manuel Torres Aguirre

---
+++ lang/es/block_navbuttons.php
@@ -0,0 +1,35 @@
+<?php
+
+$string['backgroundcolour'] = 'Color del Icono (Cualquier color v&aacute;lido en HTML)';
+$string['buttonicon'] = 'Cargue un icono personalizado (deje en blanco por omisi&oacute;n)';
+$string['buttonlink'] = 'URL para este bot&oacute;n';
+$string['buttonopenin'] = 'Enlace abre en';
+$string['buttontitle'] = 'T&iacute;tulo del bot&oacute;n';
+$string['buttontype'] = 'El bot&oacute;n va a';
+$string['buttonsenabled'] = 'Botones de navegaci&oacute;n habilitados para este curso';
+$string['coursepage'] = 'P&aacute;gina del curso';
+$string['customusebackground'] = 'Usar color para iconos personalizados';
+$string['displaybutton'] = 'Muestra este bot&oacute;n';
+$string['editsettings'] = 'Edita la configuraci&oacute;n del bot&oacute;n de navegaci&oacute;n';
+$string['extra1'] = 'Bot&oacute;n extra 1';
+$string['extra2'] = 'Bot&oacute;n extra 2';
+$string['firstbutton'] = 'Primer bot&oacute;n';
+$string['firstcourse'] = 'Primera actividad en el curso';
+$string['firstsection'] = 'Primera actividad en secci&oacute;n';
+$string['frontpage'] = 'P&aacute;gina principal';
+$string['generalsettings'] = 'Configuraci&oacute;n general';
+$string['homebutton'] = 'Bot&oacute;n de Inicio';
+$string['lastactivity'] = '&Uacute;ltima actividad';
+$string['lastactivitysection'] = '&Ucute;ltima actividad en secci&oacute;n';
+$string['lastbutton'] = '&Uacute;ltimo bot&oacute;n';
+$string['lastcourse'] = '&Uacute;ltima actividad en el curso';
+$string['lastsection'] = '&uacute;ltima actividad en la secci&oacute;n';
+$string['navbuttons'] = 'Botones de navegaci&oacute;n';
+$string['nextactivity'] = 'Siguiente actividad';
+$string['nextbutton'] = 'Siguiente bot&oacute;n';
+$string['pluginname'] = 'Navigation Buttons';
+$string['prevactivity'] = 'Actividad anterior';
+$string['prevbutton'] = 'Bot&oacute;n Previo';
+$string['selftest_ok'] = 'Navbuttons self-test: las modificaciones requeridas se han completado exitosamente';
+$string['settingsupdated'] = 'Configuraci&oacute;n actualizada';
+?>
