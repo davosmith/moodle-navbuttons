700027b3fb7b157ca286b3706a154c9ae330b15a
Davo Smith <git@davosmith.co.uk>
2019-11-18 08:23:38 +0000

    Update version.php

--- version.php
+++ version.php
@@ -16,8 +16,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018042100;
-$plugin->release = '3.4+ (Build: 2017100900)';
+$plugin->version = 2019101800;
+$plugin->release = '3.4+ (Build: 2019101800)';
 $plugin->requires = 2017111300; // Moodle 3.4.
 $plugin->cron = 0;
 $plugin->component = 'block_navbuttons';
